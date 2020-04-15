d88788dba4ae6f8a7c3664fed7597e7b71f71b3a
etchorigin <etchorigin@gmail.com>
2020-04-16 04:55:42 +0800

    Added Seeker

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import {
   H2,
   Icon,
   Intent,
+  Slider,
   Switch,
 } from "@blueprintjs/core";
 import ReactPlayer from "react-player";
 
 import "./App.css";
+
+import { formatSeconds, playedProgress } from "./TimeHelper";
 
 FocusStyleManager.onlyShowFocusOnTabs();
 document.body.className = "bp3-dark";
@@ -20,8 +23,9 @@ document.body.className = "bp3-dark";
 const initialState = {
   playing: true,
   played: 0,
-  playedSeconds: "0:00",
-  durationSeconds: "0:00",
+  playedSeconds: "",
+  durationSeconds: "",
+  seeking: false,
 };
 
 const reducer = (state, action) => {
@@ -30,8 +34,21 @@ const reducer = (state, action) => {
       return { ...state, playing: true };
     case "PAUSE":
       return { ...state, playing: false };
-    case "SET_DURATION":
+    case "SET_DURATION_SECONDS":
       return { ...state, durationSeconds: action.value };
+    case "SET_PLAYED_AND_SECONDS":
+      return {
+        ...state,
+        playedSeconds: action.seconds,
+        played: action.fraction,
+      };
+    case "SET_PLAYED_AND_SEEKING":
+      return { ...state, played: action.value, seeking: true };  
+    case "SET_PLAYED":
+      return { ...state, played: action.value };
+    case "SET_SEEKING":
+      return { ...state, seeking: action.value };
+
     default:
       throw new Error();
   }
@@ -40,13 +57,23 @@ const reducer = (state, action) => {
 function App() {
   const [state, dispatch] = React.useReducer(reducer, initialState);
   const player = React.useRef();
+  const seekingRef = React.useRef();
+  React.useEffect(() => {
+    seekingRef.current = state.seeking;
+  });
 
   const handleDurationLoad = (duration) => {
-    dispatch({ type: "SET_DURATION", value: duration });
+    dispatch({ type: "SET_DURATION_SECONDS", value: duration });
   };
 
-  const handleProgress = (state) => {
-    console.log("Progress: ", state);
+  const handleProgress = progress => {
+    if(!seekingRef.current) {
+      dispatch({
+        type: "SET_PLAYED_AND_SECONDS",
+        seconds: progress.playedSeconds,
+        fraction: progress.played * 1000,
+      });
+    }
   };
 
   const handleReset = () => {
@@ -59,13 +86,22 @@ function App() {
     dispatch({ type: state.playing ? "PAUSE" : "PLAY" });
   };
 
+  const handleSliderChange = (step) => {
+    dispatch({ type: "SET_PLAYED_AND_SEEKING", value: step });
+  };
+
+  const handleSliderRelease = () => {
+    dispatch({ type: "SET_SEEKING", value: false });
+    player.current.seekTo(state.played / 1000, "fraction");
+  };
+
   return (
     <div className="App">
       <Card elevation={Elevation.TWO} className="Top-Container">
         <ReactPlayer
           ref={player}
           url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-          height="100%"
+          height="200px"
           width="200px"
           playing={state.playing}
           onProgress={handleProgress}
@@ -75,11 +111,13 @@ function App() {
           <H6 className="Light-Font">George Zaidan | TED-Ed</H6>
           <H2>The bug that poops canday</H2>
           <div className="Details-Info-Container">
-            <div>
-              <span>
-                {state.playedSeconds} / {state.durationSeconds}
-              </span>
-            </div>
+            <span className="Details-Info-Time">
+              {formatSeconds(state.playedSeconds)}
+            </span>
+            <Icon icon="slash" iconSize={16} />
+            <span className="Details-Info-Time">
+              {formatSeconds(state.durationSeconds)}
+            </span>
             <Icon icon="symbol-circle" iconSize={6} className="Icon-Spacer" />
             <span className="Light-Font">1 language</span>
           </div>
@@ -94,6 +132,16 @@ function App() {
             <Button icon="volume-up" />
             <Button text="1x" />
           </div>
+            <Slider
+              min={0}
+              max={1000}
+              stepSize={1}
+              value={state.played}
+              labelRenderer={false}
+              onChange={handleSliderChange}
+              onRelease={handleSliderRelease}
+              className="Details-MediaControls-Slider-Container"
+            />
         </div>
       </Card>
     </div>
--- src/TimeHelper.js
+++ src/TimeHelper.js
@@ -1,1 +1,12 @@
-import { format } from "date-fns";
+const formatToString = formula => Math.round(formula).toString().padStart(2, '0');
+
+export const formatSeconds = seconds => {
+    const hours = formatToString(((seconds - seconds % 3600) / 3600) % 60);
+    const mins = formatToString(((seconds - seconds % 60) / 60) % 60);
+    const secs = formatToString(seconds % 60);
+    return `${hours}:${mins}:${secs}`
+}
+
+export const playedProgress = fraction => Math.round(fraction * 1000);
+
+export default formatSeconds;
